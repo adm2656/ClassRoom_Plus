6f18aa3b6a721d04e1e8d8ff16dc599e38b463ad
TSAI,CHENG-CHIN <adm2656@gmail.com>
2018-05-03 00:09:41 +0800

    remove card.css, fakeAuth

--- src/components/PrivateRouter.js
+++ src/components/PrivateRouter.js
@@ -1,17 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-const fakeAuth = {
-  authenticate(cb) {
-    this.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
-
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
